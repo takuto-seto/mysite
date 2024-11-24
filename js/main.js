c4acce7c2b6160d4b6e161f3e14ad21541bdad4c
t.setoguchi <t.setoguchi2611@gmail.com>
2024-11-24 15:57:40 +0900

    Create scroll js

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ function typeText() {
       const randomInterval = Math.floor(Math.random() * 350) + 50;
       setTimeout(typeText, randomInterval);
     } 
-  }
+  };
+  
+
+
+  window.addEventListener('scroll', function(){
+    // スクロール量を取得
+    const scroll = window.scrollY;
+    // 画面の高さを取得
+    const windowHeight = window.innerHeight;
+    // すべての.boxを取得
+    const boxes = document.querySelectorAll('.box');
+  
+    boxes.forEach(function(box) {
+      // boxまでの高さを取得
+      const distanceToBox = box.offsetTop;
+      // 下記条件が成り立つときだけboxにis-activeクラスを付与する
+      if(scroll + windowHeight > distanceToBox) {
+        box.classList.add('is-active');
+      }
+    });
+  }); 
